c57bb2e2ab0a9160ebdeb8931c86d5585b6eeeca
pCosta99 <costapedro.a1999@gmail.com>
2021-07-21 16:55:33 +0100

    documentation added for processor

--- dharma_server/apps/processor/lib/processor.ex
+++ dharma_server/apps/processor/lib/processor.ex
@@ -1,22 +1,21 @@
 defmodule Processor do
   @moduledoc """
-  Documentation for `Processor`.
+  A `Processor` handles info regarding one source of input.
+  It reads data from a queue, processes that same data and forwards it to processed queues.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Processor.hello()
-      :world
-
+  Starts a connection to handle one input source named `name`.
   """
-
   def start_link(name) do
     start_connection(name)
   end
 
+  @doc """
+  Used by the supervisor to spawn this process.
+  We opted to use as `id` the source from which data will be read.
+  """
+  @spec child_spec(String.t()) :: map()
   def child_spec(opts) do
     %{
       id: String.to_atom(opts),
@@ -27,41 +26,44 @@ defmodule Processor do
     }
   end
 
+  # Creates the connection and the queues that will be used in the life cycle of this process.
+  # After that, goes into a state where the process waits for messages.
   defp start_connection(source) do
     {:ok, connection} = AMQP.Connection.open
     {:ok, channel} = AMQP.Channel.open(connection)
     AMQP.Exchange.declare(channel, "dharma", :topic)
     create_queue(channel, "raw_input", ["insert.raw." <> source])
-    create_queue_no_consume(channel, "process_dashboard", ["insert.processed.*"])
-    create_queue_no_consume(channel, "process_blockchain", ["insert.processed.dharma", "insert.processed.other"])
+    create_queue(channel, "process_dashboard", ["insert.processed.*"])
+    create_queue(channel, "process_blockchain", ["insert.processed.dharma", "insert.processed.other"])
+    AMQP.Basic.consume(channel, "raw_input", nil, no_ack: true)
 
     wait_for_messages(channel)
   end
 
+  # Creates a queue named `queue_name` that's binded to each topic in the list.
+  @spec create_queue(AMQP.Channel.t(), String.t(), [String.t()]) :: :ok
   defp create_queue(channel, queue_name, topic) do
     AMQP.Queue.declare(channel, queue_name , exclusive: true)
     Enum.each(topic, fn x ->
       AMQP.Queue.bind(channel, queue_name, "dharma", routing_key: x)
     end)
-    AMQP.Basic.consume(channel, queue_name, nil, no_ack: true)
   end
 
-  defp create_queue_no_consume(channel, queue_name, topic) do
-    AMQP.Queue.declare(channel, queue_name , exclusive: true)
-    Enum.each(topic, fn x ->
-      AMQP.Queue.bind(channel, queue_name, "dharma", routing_key: x)
-    end)
-  end
-
+  # Processes the `message`, preparing it to be inserted in the processed queues.
+  # Identity for now, will change later on!
+  @spec process(any) :: any
   defp process(message) do
     message
   end
 
+  # Sends a `message` in the exchange "dharma", in a certain channel, with a `topic`.
+  @spec send(String.t(), any, AMQP.Channel.t()) :: :ok
   defp send(topic, message, channel) do
     AMQP.Basic.publish(channel, "dharma", topic, message)
     IO.puts " [x] Sent '[#{topic}] #{message}'"
   end
 
+  # A loop that waits for messages in a `channel`, processes them and re-delivers them to the processed topic.
   defp wait_for_messages(channel) do
     receive do
       {:basic_deliver, payload, meta} ->
--- dharma_server/apps/processor/lib/processor/application.ex
+++ dharma_server/apps/processor/lib/processor/application.ex
@@ -1,18 +1,17 @@
 defmodule Processor.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   @impl true
   def start(_type, _args) do
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Processor.Supervisor]
-    read_children() |> Supervisor.start_link(opts)
+    read_children |> Supervisor.start_link(opts)
   end
 
+  # Reads the children that must be spawned (to read from each source) from the environment.
+  # Returns a list with tuples of {Processor, source} for each source read.
+  @spec read_children :: [{atom(), String.t()}]
   defp read_children do
     Application.fetch_env!(:processor, :source)
     |> Enum.map(fn x -> {Processor, x} end)
