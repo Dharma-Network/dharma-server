808d0131ba61ae1abece570b49ddb1b1f96b16ea
tiago <tiagohomagalhaes@gmail.com>
2021-07-21 16:58:18 +0100

    comment connector

--- dharma_server/apps/connector/lib/connector.ex
+++ dharma_server/apps/connector/lib/connector.ex
@@ -1,13 +1,27 @@
 defmodule Connector do
   @moduledoc """
-  Documentation for `Connector`.
+  This is the `Connector` module.
+  This module aims to receive raw data from
+  external data sources and
+  publish it to a RabbitMQ exchange.
   """
+
+  @moduledoc since: "1.0.0"
+
   use GenServer
+
+  @doc """
+  Convenience method for startup.
+  """
+  @spec start_link(any) :: :ignore | {:error, any} | {:ok, pid}
 
   def start_link(_opts) do
     GenServer.start_link(__MODULE__, [], [{:name, __MODULE__}])
   end
 
+  @doc """
+  Callbacks for GenServer.Behaviour.
+  """
   @impl true
   def init([]) do
     pid = spawn(fn -> Extractor.loop  end)
@@ -16,6 +30,9 @@ defmodule Connector do
     {:ok,{connection, channel, pid}}
   end
 
+  @doc """
+  A send request, expects a source and a message.
+  """
   @impl true
   def handle_call({:send, source, message}, _from, {_connection, channel, _pid} = state) do
     routing = "insert.raw." <> source
@@ -23,10 +40,15 @@ defmodule Connector do
     {:reply, state, state}
   end
 
+  @doc """
+  A pid request.
+  """
   @impl true
   def handle_call(:pid, _from, {_connection, _channel, pid} = state) do
     {:reply, pid, state}
   end
+
+  # Start a connection with RabbitMQ and declare an exchange.
 
   defp start_connection do
     {:ok, connection} = AMQP.Connection.open
@@ -35,11 +57,16 @@ defmodule Connector do
     {connection, channel}
   end
 
+  # Publishes a message to an Exchange.
+  @spec send(String.t, String.t, AMQP.Channel.t()) :: :ok
+
   defp send(topic, message, channel) do
     AMQP.Basic.publish(channel, "dharma", topic, message)
     IO.puts " [x] Sent '[#{topic}] #{message}'"
   end
 
+  # Close RabbitMQ connection.
+  @spec close_connection(AMQP.Connection.t()) :: :ok | {:error, any()}
   defp close_connection(connection) do
     AMQP.Connection.close(connection)
   end
