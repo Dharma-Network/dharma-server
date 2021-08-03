29b05024b428bd66667bb765d03235714df7901f
pCosta99 <costapedro.a1999@gmail.com>
2021-08-03 16:45:35 +0100

    added plug for authentication in Database.Operations

--- apps/database/lib/database/operations.ex
+++ apps/database/lib/database/operations.ex
@@ -6,13 +6,12 @@ defmodule Database.Operations do
 
   plug(Tesla.Middleware.BaseUrl, @url)
   plug(Tesla.Middleware.JSON)
+  plug(Tesla.Middleware.Headers, [{"cookie", Database.Auth.get_cookie()}])
   adapter(Tesla.Adapter.Finch, name: FinchAdapter)
 
   def get_github_sources() do
-    client = client()
-
     body = %{selector: %{project_type: %{"$eq": "github"}}, fields: ["list_of_urls"]}
-    {:ok, resp} = post(client, @name_db <> "/_find", body)
+    {:ok, resp} = post(@name_db <> "/_find", body)
 
     resp.body["docs"]
     |> Enum.flat_map(fn doc ->
@@ -24,15 +23,8 @@ defmodule Database.Operations do
     |> Enum.into(%{}, & &1)
   end
 
-  defp client do
-    Tesla.client([
-      {Tesla.Middleware.Headers, [{"cookie", Database.Auth.get_cookie()}]}
-    ])
-  end
-
   # TODO: Don't rely on couchdb UUID
   def post(body) do
     post("/" <> @name_db, body)
   end
-
 end
