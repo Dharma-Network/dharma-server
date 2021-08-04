64b4788039f5a25d68423acb17c9cb2c8cb93dd7
tiago <tiagohomagalhaes@gmail.com>
2021-08-04 14:34:32 +0100

    Add config vars and small fixes

--- apps/database/lib/database/auth.ex
+++ apps/database/lib/database/auth.ex
@@ -4,39 +4,41 @@ defmodule Database.Auth do
   use Joken.Config
 
   @url Application.fetch_env!(:database, :url_db)
+  @jwt_secret Application.fetch_env!(:database, :jwt_secret)
+  @sign_alg "HS256"
+  @exp_claim_time 14
+  @sub_user "admin"
 
   plug(Tesla.Middleware.BaseUrl, @url)
   plug(Tesla.Middleware.JSON)
 
   adapter(Tesla.Adapter.Finch, name: FinchAdapter)
 
-  @sign_alg "HS256"
   def start_link(_opts) do
-    Agent.start_link(fn -> retrieve_jwt() end, name: __MODULE__)
+    Agent.start_link(fn -> retrieve_auth() end, name: __MODULE__)
   end
 
   # Set exp claim.
-  def token_config() do
+  defp token_config() do
     default_claims(
       skip: [:iss, :aud, :jti, :adu, :nbf, :iat],
-      default_exp: 3
+      default_exp: @exp_claim_time * 60 * 60 * 24
     )
-    |> add_claim("sub", fn -> "admin" end, &(&1 == "admin"))
+    |> add_claim("sub", fn -> @sub_user end, &(&1 == @sub_user))
   end
 
-  def retrieve_jwt() do
-    signer = Joken.Signer.create(@sign_alg, "password")
-    {:ok, token, _claims} = generate_and_sign(%{}, signer) |> IO.inspect()
-    Joken.expand(token) |> IO.inspect()
-    IO.puts("Retrieve jwt")
+  defp retrieve_auth() do
+    signer = Joken.Signer.create(@sign_alg, @jwt_secret)
+    {:ok, token, _claims} = generate_and_sign(%{}, signer)
+    Joken.expand(token)
     token
   end
 
-  def get_jwt do
+  def get_auth do
     Agent.get(__MODULE__, & &1)
   end
 
-  def refresh_jwt() do
-    Agent.update(__MODULE__, fn _ -> retrieve_jwt() end)
+  def refresh_auth() do
+    Agent.update(__MODULE__, fn _ -> retrieve_auth() end)
   end
 end
--- apps/database/lib/database/operations.ex
+++ apps/database/lib/database/operations.ex
@@ -6,14 +6,12 @@ defmodule Database.Operations do
 
   plug(Tesla.Middleware.BaseUrl, @url)
   plug(Tesla.Middleware.JSON)
-  # plug(Tesla.Middleware.Headers, [{"Authorization", "Bearer " <> Database.Auth.get_jwt()}])
 
   adapter(Tesla.Adapter.Finch, name: FinchAdapter)
 
   def get_github_sources() do
     body = %{selector: %{project_type: %{"$eq": "github"}}, fields: ["list_of_urls"]}
     {:ok, resp} = post_with_retry(@name_db <> "/_find", body)
-    IO.inspect(resp, label: "get_gith")
 
     resp.body["docs"]
     |> Enum.flat_map(fn doc ->
@@ -25,18 +23,13 @@ defmodule Database.Operations do
     |> Enum.into(%{}, & &1)
   end
 
-  defp post_with_retry(client, path, body) do
-    post(client, path, body)
-  end
-
   defp post_with_retry(path, body) do
     {:ok, resp} = post(client(), path, body)
 
     case resp.status do
       401 ->
-        IO.puts("Refresh")
-        Database.Auth.refresh_jwt()
-        post_with_retry(client(), path, body)
+        Database.Auth.refresh_auth()
+        post_with_retry(path, body)
 
       _ ->
         {:ok, resp}
@@ -45,7 +38,7 @@ defmodule Database.Operations do
 
   defp client do
     middleware = [
-      {Tesla.Middleware.Headers, [{"Authorization", "Bearer " <> Database.Auth.get_jwt()}]}
+      {Tesla.Middleware.Headers, [{"Authorization", "Bearer " <> Database.Auth.get_auth()}]}
     ]
 
     Tesla.client(middleware)
